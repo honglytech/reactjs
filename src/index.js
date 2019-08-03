70ec95a1410767f1b8a94eac4cd22db6d66bb845
lyhd <lyhong.rupp@gmail.com>
2019-08-03 20:12:05 +1000

    rendered json file and removed duplicate objects

--- src/index.js
+++ src/index.js
@@ -25,7 +25,25 @@ class App extends React.Component {
     this.setState({ value: event.target.value });
   };
 
+  getUnique(arr, comp) {
+    const unique = arr
+      //store the comparison values in array
+      .map(e => e[comp])
+
+      // store the keys of the unique objects
+      .map((e, i, final) => final.indexOf(e) === i && i)
+
+      // eliminate the dead keys & store unique objects
+      .filter(e => arr[e])
+
+      .map(e => arr[e]);
+
+    return unique;
+  }
+
   render() {
+    const countries = require("./countries.json");
+    const uniqueCountry = this.getUnique(countries.world, "country");
     return (
       <form onSubmit={this.handleSubmit}>
         <label>
@@ -43,6 +61,20 @@ class App extends React.Component {
           Looping through Array
           <select>
             {this.state.countries.map(item => (
+              <option key={item.id} value={item.country}>
+                {item.country}
+              </option>
+            ))}
+            {console.log(this.state.countries)}
+          </select>
+        </label>
+        <input type="submit" value="Submit" />
+        <br />
+        <br />
+        <label>
+          Looping through Json File
+          <select>
+            {uniqueCountry.map(item => (
               <option key={item.id} value={item.country}>
                 {item.country}
               </option>
