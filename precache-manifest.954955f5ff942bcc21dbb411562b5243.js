8293fce6240925310890d3b2d90dabd2ebe103b8
lyhd <lyhong.rupp@gmail.com>
2019-08-10 20:15:38 +1000

    Updates

---
+++ precache-manifest.954955f5ff942bcc21dbb411562b5243.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b33077dd0ec3ffc59428aef0d41990f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b42aa7dd048f4b5d252a",
+    "url": "/static/js/2.5c4e487f.chunk.js"
+  },
+  {
+    "revision": "54b992b868db5adc61d6",
+    "url": "/static/js/main.e02014dc.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
