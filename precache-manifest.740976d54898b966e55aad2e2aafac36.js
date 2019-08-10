cfbf2018f1ec3814e3eb066fadc06b2ae007c4a2
lyhd <lyhong.rupp@gmail.com>
2019-08-10 20:48:14 +1000

    Updates

---
+++ precache-manifest.740976d54898b966e55aad2e2aafac36.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "863673517f4171dd083b3f2eaac7bc5a",
+    "url": "/reactjs/index.html"
+  },
+  {
+    "revision": "b42aa7dd048f4b5d252a",
+    "url": "/reactjs/static/js/2.5c4e487f.chunk.js"
+  },
+  {
+    "revision": "54b992b868db5adc61d6",
+    "url": "/reactjs/static/js/main.e02014dc.chunk.js"
+  },
+  {
+    "revision": "984e2b0c9d0708ff072f",
+    "url": "/reactjs/static/js/runtime~main.71c88981.js"
+  }
+]);
