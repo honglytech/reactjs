c1bae6d65a378ff6cee7afc5a81a1e7b0ee05fc4
lyhd <lyhong.rupp@gmail.com>
2019-11-30 19:35:30 +1100

    a new react app (reactnetlify)

--- src/index.js
+++ src/index.js
@@ -1,44 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { value: "coconut" };
+ReactDOM.render(<App />, document.getElementById("root"));
 
-    // this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  //   handleChange(event) {
-  //     this.setState({ value: event.target.value });
-  //   }
-
-  handleSubmit(event) {
-    alert("Your favorite flavor is: " + this.state.value);
-    event.preventDefault();
-  }
-
-  handleChange = event => {
-    this.setState({ value: event.target.value });
-  };
-
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Pick your favorite flavor:
-          <select value={this.state.value} onChange={this.handleChange}>
-            <option value="grapefruit">Grapefruit</option>
-            <option value="lime">Lime</option>
-            <option value="coconut">Coconut</option>
-            <option value="mango">Mango</option>
-          </select>
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
-    );
-  }
-}
-
-ReactDOM.render(<App />, document.querySelector("#root"));
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
