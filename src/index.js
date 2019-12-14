aa78d2028ca8262f7b28eb0d31f1dc558c565073
lyhd <lyhong.rupp@gmail.com>
2019-12-14 19:59:43 +1100

    created card flip

--- src/index.js
+++ src/index.js
@@ -1,139 +1,44 @@
-import React, { useMemo, useEffect, useState } from "react";
 import ReactDOM from "react-dom";
-import { useDropzone } from "react-dropzone";
+import React, { useState } from "react";
+import ReactCardFlip from "react-card-flip";
 
-const baseStyle = {
-  flex: 1,
-  display: "flex",
-  flexDirection: "column",
-  alignItems: "center",
-  padding: "20px",
-  borderWidth: 2,
-  borderRadius: 2,
-  borderColor: "#eeeeee",
-  borderStyle: "dashed",
-  backgroundColor: "#fafafa",
-  color: "#bdbdbd",
-  outline: "none",
-  transition: "border .24s ease-in-out"
-};
+const CardFlip = () => {
+  const [isFlipped, setIsFlipped] = useState(false);
 
-const activeStyle = {
-  borderColor: "#2196f3"
-};
-
-const acceptStyle = {
-  borderColor: "#00e676"
-};
-
-const rejectStyle = {
-  borderColor: "#ff1744"
-};
-
-const thumbsContainer = {
-  display: "flex",
-  flexDirection: "row",
-  flexWrap: "wrap",
-  marginTop: 16
-};
-
-const thumb = {
-  display: "inline-flex",
-  borderRadius: 2,
-  border: "1px solid #eaeaea",
-  marginBottom: 8,
-  marginRight: 8,
-  width: "auto",
-  height: 200,
-  padding: 4,
-  boxSizing: "border-box"
-};
-
-const thumbInner = {
-  display: "flex",
-  minWidth: 0,
-  overflow: "hidden"
-};
-
-const img = {
-  display: "block",
-  width: "auto",
-  height: "100%"
-};
-
-function StyledDropzone(props) {
-  const [files, setFiles] = useState([]);
-  const {
-    getRootProps,
-    getInputProps,
-    isDragActive,
-    isDragAccept,
-    isDragReject,
-    acceptedFiles,
-    open
-  } = useDropzone({
-    accept: "image/*",
-    noClick: true,
-    noKeyboard: true,
-    onDrop: acceptedFiles => {
-      setFiles(
-        acceptedFiles.map(file =>
-          Object.assign(file, {
-            preview: URL.createObjectURL(file)
-          })
-        )
-      );
-    }
-  });
-
-  const style = useMemo(
-    () => ({
-      ...baseStyle,
-      ...(isDragActive ? activeStyle : {}),
-      ...(isDragAccept ? acceptStyle : {}),
-      ...(isDragReject ? rejectStyle : {})
-    }),
-    [isDragActive, isDragReject]
-  );
-
-  const thumbs = files.map(file => (
-    <div style={thumb} key={file.name}>
-      <div style={thumbInner}>
-        <img src={file.preview} style={img} />
-      </div>
-    </div>
-  ));
-
-  useEffect(
-    () => () => {
-      // Make sure to revoke the data uris to avoid memory leaks
-      files.forEach(file => URL.revokeObjectURL(file.preview));
-    },
-    [files]
-  );
-
-  const filepath = acceptedFiles.map(file => (
-    <li key={file.path}>
-      {file.path} - {file.size} bytes
-    </li>
-  ));
-
+  const handleClick = () => {
+    setIsFlipped(!isFlipped);
+  };
   return (
-    <div className="container">
-      <div {...getRootProps({ style })}>
-        <input {...getInputProps()} />
-        <p>Drag 'n' drop some files here</p>
-        <button type="button" onClick={open}>
-          Open File Dialog
-        </button>
+    <ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal">
+      <div
+        style={{
+          backgroundColor: "green",
+          height: 100,
+          color: "#fff",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center"
+        }}
+      >
+        How do you define the meaning of success?
+        <button onClick={handleClick}>Click to flip</button>
       </div>
-      <aside>
-        <h4>Files</h4>
-        <ul>{filepath}</ul>
-      </aside>
-      <aside style={thumbsContainer}>{thumbs}</aside>
-    </div>
-  );
-}
 
-ReactDOM.render(<StyledDropzone />, document.getElementById("root"));
+      <div
+        style={{
+          backgroundColor: "red",
+          height: 100,
+          color: "#fff",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center"
+        }}
+      >
+        Create video tutorials on YouTube
+        <button onClick={handleClick}>Click to flip</button>
+      </div>
+    </ReactCardFlip>
+  );
+};
+
+ReactDOM.render(<CardFlip />, document.querySelector("#root"));
