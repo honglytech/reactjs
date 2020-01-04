673f5d877710f582d17f2d92baa565468389cdae
lyhd <lyhong.rupp@gmail.com>
2020-01-04 15:40:43 +1100

    installed google charts library with example

--- src/index.js
+++ src/index.js
@@ -1,44 +1,30 @@
-import ReactDOM from "react-dom";
-import React, { useState } from "react";
-import ReactCardFlip from "react-card-flip";
+import { Chart } from "react-google-charts";
+import * as React from "react";
+import { render } from "react-dom";
 
-const CardFlip = () => {
-  const [isFlipped, setIsFlipped] = useState(false);
-
-  const handleClick = () => {
-    setIsFlipped(!isFlipped);
-  };
+const GoogleCharts = () => {
   return (
-    <ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal">
-      <div
-        style={{
-          backgroundColor: "green",
-          height: 100,
-          color: "#fff",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center"
-        }}
-      >
-        How do you define the meaning of success?
-        <button onClick={handleClick}>Click to flip</button>
-      </div>
-
-      <div
-        style={{
-          backgroundColor: "red",
-          height: 100,
-          color: "#fff",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center"
-        }}
-      >
-        Create video tutorials on YouTube
-        <button onClick={handleClick}>Click to flip</button>
-      </div>
-    </ReactCardFlip>
+    <Chart
+      width={"500px"}
+      height={"300px"}
+      chartType="GeoChart"
+      data={[
+        ["Country", "Popularity"],
+        ["Germany", 200],
+        ["United States", 300],
+        ["Brazil", 400],
+        ["Canada", 500],
+        ["France", 600],
+        ["RU", 700],
+        ["Cambodia", 800],
+        ["Australia", 100]
+      ]}
+      // Note: you will need to get a mapsApiKey for your project.
+      // See: https://developers.google.com/chart/interactive/docs/basic_load_libs#load-settings
+      mapsApiKey="YOUR_KEY_HERE"
+      rootProps={{ "data-testid": "1" }}
+    />
   );
 };
 
-ReactDOM.render(<CardFlip />, document.querySelector("#root"));
+render(<GoogleCharts />, document.querySelector("#root"));
