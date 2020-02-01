ca97eeb441517e2d55e0e7df7de614e8e44bbe53
lyhd <lyhong.rupp@gmail.com>
2020-02-01 20:10:32 +1100

    implemented react calendar

--- src/index.js
+++ src/index.js
@@ -1,30 +1,21 @@
-import { Chart } from "react-google-charts";
-import * as React from "react";
+import React, { useState } from "react";
 import { render } from "react-dom";
+import Calendar from "react-calendar";
 
-const GoogleCharts = () => {
+const ReactCalendar = () => {
+  const [date, setDate] = useState(new Date());
+
+  const onChange = date => {
+    setDate(date);
+  };
+
   return (
-    <Chart
-      width={"500px"}
-      height={"300px"}
-      chartType="GeoChart"
-      data={[
-        ["Country", "Popularity"],
-        ["Germany", 200],
-        ["United States", 300],
-        ["Brazil", 400],
-        ["Canada", 500],
-        ["France", 600],
-        ["RU", 700],
-        ["Cambodia", 800],
-        ["Australia", 100]
-      ]}
-      // Note: you will need to get a mapsApiKey for your project.
-      // See: https://developers.google.com/chart/interactive/docs/basic_load_libs#load-settings
-      mapsApiKey="YOUR_KEY_HERE"
-      rootProps={{ "data-testid": "1" }}
-    />
+    <div>
+      <Calendar showWeekNumbers onChange={onChange} value={date} />
+      {console.log(date)}
+      {date.toString()}
+    </div>
   );
 };
 
-render(<GoogleCharts />, document.querySelector("#root"));
+render(<ReactCalendar />, document.querySelector("#root"));
