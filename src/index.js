1c2df8eb87524f6930e0bcff646945c7bacf4672
lyhd <lyhong.rupp@gmail.com>
2020-02-29 21:57:19 +1100

    posted random quote to facebook page

--- src/index.js
+++ src/index.js
@@ -1,36 +1,55 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { render } from "react-dom";
-import FacebookLogin from "react-facebook-login";
+import axios from "axios";
 
-const ReactFacebookLogin = () => {
-  const [accessToken, setAccessToken] = useState("");
+const ReactFBPageRandomQuote = () => {
+  const [quote, setQuote] = useState("");
 
-  const componentClicked = data => {
-    console.log("data", data);
-  };
+  // useEffect(() => {
+  //   axios.get("https://quotes.rest/qod?language=en").then(res => {
+  //     const result = res.data;
+  //     console.log(result);
+  //     setQuote(result.contents.quotes[0].quote);
+  //   });
+  // });
 
-  const responseFacebook = response => {
-    // console.log(response.accessToken);
-    setAccessToken(response.accessToken);
+  useEffect(() => {
+    axios.get("https://api.quotable.io/random").then(res => {
+      const result = res.data;
+      console.log(result.content);
+      setQuote(result.content);
+    });
+  }, []);
+
+  const postRandomQuote = () => {
+    axios
+      .post("https://graph.facebook.com/1515470308497082/feed?", {
+        message: quote,
+        access_token:
+          "EAAIzQ3pdeDsBACpoudBhcQoDWXgi3pYrqjZAf3ZCfq49fSOpzQvTZC39G2Or9DbcU152eA2ZBlc83fOeot2ZC6mZBYav7y1cxGLekJOUeXZBOsI51yiYPqHDysyfF5k6zx5ftTiuA4FJGFSMfPG7U9vTXhLSnNblKL2ndS6yuZBCNAZDZD"
+      })
+      .then(
+        res => {
+          const result = res.data;
+          console.log(result);
+          alert("Success!");
+        },
+        error => {
+          console.log(error);
+        }
+      );
   };
 
   return (
     <div>
-      React Facebook Login
+      React Facebook Post Page Status - Random Quote
       <br />
-      User Short-Lived Access Token:
       <br />
-      {accessToken}
+      {quote}
       <br />
-      <FacebookLogin
-        appId="1614986128648687"
-        autoLoad={true}
-        fields="name,email,picture"
-        onClick={componentClicked}
-        callback={responseFacebook}
-      />
+      <button onClick={() => postRandomQuote()}>Post Status</button>
     </div>
   );
 };
 
-render(<ReactFacebookLogin />, document.querySelector("#root"));
+render(<ReactFBPageRandomQuote />, document.querySelector("#root"));
