8a839f1e56df12d595d6b78e709f0236d7bcf5ab
lyhd <lyhong.rupp@gmail.com>
2020-08-08 20:50:13 +1000

    built a simple AI generated photos app using API

--- src/index.js
+++ src/index.js
@@ -1,57 +1,38 @@
-import React, { useRef } from "react";
+import React, { useEffect, useState } from "react";
 import { render } from "react-dom";
-import { useReactToPrint } from "react-to-print";
-
-class ComponentToPrint extends React.Component {
-  render() {
-    return (
-      <div>
-        <div style={{ fontSize: "40px", color: "green" }}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Pretium
-          viverra suspendisse potenti nullam ac tortor vitae purus faucibus. Eu
-          lobortis elementum nibh tellus. Urna molestie at elementum eu
-          facilisis sed odio morbi quis. Et molestie ac feugiat sed lectus
-          vestibulum mattis ullamcorper. Ut tellus elementum sagittis vitae et.
-          Leo urna molestie at elementum. Vestibulum rhoncus est pellentesque
-          elit ullamcorper dignissim. Sollicitudin nibh sit amet commodo nulla
-          facilisi. Amet luctus venenatis lectus magna. Ultricies integer quis
-          auctor elit sed vulputate mi.
-        </div>
-        <div style={{ color: "blue" }}>
-          Characteristics of active learning 1. You look for ways of being more
-          involved in what you are learning. 2. You are engaged in the whole
-          learning process (and in a position to see why information has been
-          selected). 3. You look for links between different things that you
-          discover. 4. You make a conscious effort to make sense of, and find
-          meaning in, what you learn. Understanding is usually deeper. 5. You
-          are involved in reflection and self-evaluation. 6. Your attention span
-          is longer because your mind is more fully engaged. 7. Long-term memory
-          is assisted. If you understand what you learn, and keep relating what
-          you learn to what you already know, you are more likely to remember
-          what you have learnt. 8. Linking information helps you to see how you
-          can apply it to different situations. 9. Learning is personalised and
-          interesting.
-        </div>
-        <img
-          width="100%"
-          src="https://honglytech.com/wp-content/uploads/2020/06/laptop-coffee-arm-1205256.jpg"
-        />
-      </div>
-    );
-  }
-}
+import axios from "axios";
 
 const Example = () => {
-  const componentRef = useRef();
-  const handlePrint = useReactToPrint({
-    content: () => componentRef.current,
-  });
+  const [image, setImage] = useState("");
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.generated.photos/api/v1/faces?version=3&gender=female&age=young-adult&ethnicity=asian",
+        {
+          headers: {
+            Authorization: `API-Key 6JiyTxpVXEFZkGYGjRYCXw`,
+          },
+        }
+      )
+      .then((res) => {
+        console.log(res.data.faces[0].urls[4][512]);
+        setImage(res.data.faces[0].urls[4][512]);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }, []);
 
   return (
-    <div>
-      <ComponentToPrint ref={componentRef} />
-      <button onClick={handlePrint}>Print this out!</button>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItmes: "center",
+      }}
+    >
+      <img src={image} />
     </div>
   );
 };
