c20f7534764e3d14954f1c3f56c938fc55864cfd
lyhd <lyhong.rupp@gmail.com>
2020-09-05 20:53:17 +1000

    built a compound interest calculator

--- src/index.js
+++ src/index.js
@@ -1,31 +1,18 @@
 import React, { useState } from "react";
 import { render } from "react-dom";
-import axios from "axios";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
 
-const WeatherApp = () => {
-  const [temperature, setTemperature] = useState("");
-  const [desc, setDesc] = useState("");
-  const [city, setCity] = useState("Melbourne");
-  const [country, setCountry] = useState("AU");
+const CompoundInterest = () => {
+  const [principal, setPrincipal] = useState();
+  const [years, setYears] = useState();
+  const [interest, setInterest] = useState();
+  const [result, setResult] = useState();
 
-  const getWeatherData = (city, country) => {
-    axios({
-      method: "GET",
-      url: `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=180941f68139fba12f166dc35d9b688b`,
-    })
-      .then((response) => {
-        console.log(response.data.main.temp);
-        // Kelvin to Fahrenheit
-        // setTemperature((response.data.main.temp - 273.15) * 1.8 + 32);
-
-        // Kelvin to Celsius
-        setTemperature(response.data.main.temp - 273.15);
-        // console.log(response.data);
-        setDesc(response.data.weather[0].main);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+  const calculate = () => {
+    // A=p(1+(r/n))^(nt)
+    const result = principal * Math.pow(1 + interest, years);
+    setResult(result.toFixed(2));
   };
 
   return (
@@ -33,61 +20,48 @@ const WeatherApp = () => {
       <div
         style={{
           display: "flex",
-          justifyContent: "center",
           alignItems: "center",
-          height: "70px",
-          width: "100%",
-          backgroundColor: "#226ba3",
-          fontSize: "30px",
-          color: "#fff",
+          justifyContent: "center",
+          height: "40vh",
         }}
       >
-        Weather APP
-      </div>
-      {/* <div
-        style={{ height: "5px", width: "100%", backgroundColor: "blue" }}
-      ></div> */}
-      <br />
-      <div style={{ marginLeft: "33%" }}>
-        <div
-          style={{
-            height: "150px",
-            width: "450px",
-            backgroundColor: "#94e5ff",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            fontSize: "25px",
-          }}
-        >
-          {new Date().toLocaleString()}
+        <form>
+          <TextField
+            label="What is the principal?"
+            variant="outlined"
+            type="number"
+            onChange={(e) => setPrincipal(e.target.value)}
+          />
+          <TextField
+            label="How many years?"
+            variant="outlined"
+            type="number"
+            onChange={(e) => setYears(e.target.value)}
+          />
+          <TextField
+            label="Annual Interest rate?"
+            variant="outlined"
+            type="number"
+            onChange={(e) => setInterest(e.target.value / 100)}
+          />
           <br />
-          {city} Weather
           <br />
-          {/* {Math.round(temperature * 100) / 100} ℉ */}
-          {Math.round(temperature * 100) / 100} ℃ - {desc}
-        </div>
-        <br />
-        <input
-          type="text"
-          value={city}
-          onChange={(e) => setCity(e.target.value)}
-        />
-        <input
-          type="text"
-          value={country}
-          onChange={(e) => setCountry(e.target.value)}
-        />
-        <button
-          onClick={() => {
-            getWeatherData(city, country);
-          }}
-        >
-          GET
-        </button>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={() => {
+              calculate();
+            }}
+          >
+            Calculate
+          </Button>
+          <br />
+          <br />
+          <div style={{ fontSize: "30px" }}>${result}</div>
+        </form>
       </div>
     </>
   );
 };
 
-render(<WeatherApp />, document.querySelector("#root"));
+render(<CompoundInterest />, document.querySelector("#root"));
