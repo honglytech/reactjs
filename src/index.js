951f4e003621150d67320e2c92dc2231ecb8dff7
lyhd <lyhong.rupp@gmail.com>
2020-09-12 20:30:45 +1000

    installed and tested jaker.js with material ui

--- src/index.js
+++ src/index.js
@@ -1,23 +1,58 @@
-import React, { useState } from "react";
+import React from "react";
 import { render } from "react-dom";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
+import faker from "faker";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Typography from "@material-ui/core/Typography";
 
-const CompoundInterest = () => {
-  const [principal, setPrincipal] = useState();
-  const [years, setYears] = useState();
-  const [interest, setInterest] = useState();
-  const [result, setResult] = useState();
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 240,
+  },
+});
 
-  const calculate = () => {
-    // A=p(1+(r/n))^(nt)
-    const result = principal * Math.pow(1 + interest, years);
-    setResult(result.toFixed(2));
-  };
+const Faker = () => {
+  const classes = useStyles();
 
   return (
     <>
-      <div
+      <Card className={classes.root}>
+        <CardActionArea>
+          <CardMedia
+            className={classes.media}
+            image={faker.image.image()}
+            title="Contemplative Reptile"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              {faker.finance.accountName()}
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+              {faker.finance.currencySymbol()}
+              {faker.finance.amount()}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button size="small" color="primary">
+            Share
+          </Button>
+          <Button size="small" color="primary">
+            Learn More
+          </Button>
+        </CardActions>
+      </Card>
+
+      {/* <div
         style={{
           display: "flex",
           alignItems: "center",
@@ -27,41 +62,52 @@ const CompoundInterest = () => {
       >
         <form>
           <TextField
-            label="What is the principal?"
+            label="First name"
             variant="outlined"
-            type="number"
-            onChange={(e) => setPrincipal(e.target.value)}
+            placeholder={faker.name.firstName()}
           />
           <TextField
-            label="How many years?"
+            label="Last name"
             variant="outlined"
-            type="number"
-            onChange={(e) => setYears(e.target.value)}
+            placeholder={faker.name.lastName()}
           />
           <TextField
-            label="Annual Interest rate?"
+            label="Job title"
             variant="outlined"
-            type="number"
-            onChange={(e) => setInterest(e.target.value / 100)}
+            placeholder={faker.name.jobTitle()}
+          />
+          <TextField
+            label="Street address"
+            variant="outlined"
+            placeholder={faker.address.streetAddress()}
+          />
+          <TextField
+            label="Country"
+            variant="outlined"
+            placeholder={faker.address.country()}
           />
           <br />
           <br />
-          <Button
-            variant="contained"
-            color="primary"
-            onClick={() => {
-              calculate();
-            }}
-          >
-            Calculate
+          <TextField
+            label="Phone number"
+            variant="outlined"
+            placeholder={faker.phone.phoneNumber()}
+          />
+          <TextField
+            label="Email"
+            variant="outlined"
+            placeholder={faker.internet.email()}
+          />
+          <br />
+          <br />
+          <Button variant="contained" color="primary" onClick={() => {}}>
+            Submit
           </Button>
           <br />
-          <br />
-          <div style={{ fontSize: "30px" }}>${result}</div>
         </form>
-      </div>
+      </div> */}
     </>
   );
 };
 
-render(<CompoundInterest />, document.querySelector("#root"));
+render(<Faker />, document.querySelector("#root"));
