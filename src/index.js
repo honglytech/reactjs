c8856cf29dedcadf3d86f850a186321e8ad2f5a1
lyhd <lyhong.rupp@gmail.com>
2020-09-19 18:57:55 +1000

    installed ant design library & implemented the layout with slider

--- src/index.js
+++ src/index.js
@@ -1,113 +1,132 @@
-import React from "react";
+import React, { useState } from "react";
 import { render } from "react-dom";
-import TextField from "@material-ui/core/TextField";
-import Button from "@material-ui/core/Button";
-import faker from "faker";
-import { makeStyles } from "@material-ui/core/styles";
-import Card from "@material-ui/core/Card";
-import CardActionArea from "@material-ui/core/CardActionArea";
-import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
-import Typography from "@material-ui/core/Typography";
+import { Layout, Menu, Breadcrumb } from "antd";
+import {
+  DesktopOutlined,
+  PieChartOutlined,
+  FileOutlined,
+  TeamOutlined,
+  UserOutlined,
+} from "@ant-design/icons";
+import "./index.css";
+import "antd/dist/antd.css";
 
-const useStyles = makeStyles({
-  root: {
-    maxWidth: 345,
-  },
-  media: {
-    height: 240,
-  },
-});
+const { Header, Content, Footer, Sider } = Layout;
+const { SubMenu } = Menu;
 
-const Faker = () => {
-  const classes = useStyles();
+const AntDesign = () => {
+  const [collapsed, setCollapsed] = useState(false);
+
+  const onCollapse = (collapsed) => {
+    setCollapsed(!collapsed);
+  };
 
   return (
-    <>
-      <Card className={classes.root}>
-        <CardActionArea>
-          <CardMedia
-            className={classes.media}
-            image={faker.image.image()}
-            title="Contemplative Reptile"
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="h2">
-              {faker.finance.accountName()}
-            </Typography>
-            <Typography variant="body2" color="textSecondary" component="p">
-              {faker.finance.currencySymbol()}
-              {faker.finance.amount()}
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-        <CardActions>
-          <Button size="small" color="primary">
-            Share
-          </Button>
-          <Button size="small" color="primary">
-            Learn More
-          </Button>
-        </CardActions>
-      </Card>
-
-      {/* <div
-        style={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          height: "40vh",
-        }}
-      >
-        <form>
-          <TextField
-            label="First name"
-            variant="outlined"
-            placeholder={faker.name.firstName()}
-          />
-          <TextField
-            label="Last name"
-            variant="outlined"
-            placeholder={faker.name.lastName()}
-          />
-          <TextField
-            label="Job title"
-            variant="outlined"
-            placeholder={faker.name.jobTitle()}
-          />
-          <TextField
-            label="Street address"
-            variant="outlined"
-            placeholder={faker.address.streetAddress()}
-          />
-          <TextField
-            label="Country"
-            variant="outlined"
-            placeholder={faker.address.country()}
-          />
-          <br />
-          <br />
-          <TextField
-            label="Phone number"
-            variant="outlined"
-            placeholder={faker.phone.phoneNumber()}
-          />
-          <TextField
-            label="Email"
-            variant="outlined"
-            placeholder={faker.internet.email()}
-          />
-          <br />
-          <br />
-          <Button variant="contained" color="primary" onClick={() => {}}>
-            Submit
-          </Button>
-          <br />
-        </form>
-      </div> */}
-    </>
+    <div>
+      <Layout style={{ minHeight: "100vh" }}>
+        <Sider collapsible collapsed={collapsed} onCollapse={onCollapse}>
+          <div className="logo" />
+          <Menu theme="dark" defaultSelectedKeys={["1"]} mode="inline">
+            <Menu.Item key="1" icon={<PieChartOutlined />}>
+              Option 1
+            </Menu.Item>
+            <Menu.Item key="2" icon={<DesktopOutlined />}>
+              Option 2
+            </Menu.Item>
+            <SubMenu key="sub1" icon={<UserOutlined />} title="User">
+              <Menu.Item key="3">Tom</Menu.Item>
+              <Menu.Item key="4">Bill</Menu.Item>
+              <Menu.Item key="5">Alex</Menu.Item>
+            </SubMenu>
+            <SubMenu key="sub2" icon={<TeamOutlined />} title="Team">
+              <Menu.Item key="6">Team 1</Menu.Item>
+              <Menu.Item key="8">Team 2</Menu.Item>
+            </SubMenu>
+            <Menu.Item key="9" icon={<FileOutlined />} />
+          </Menu>
+        </Sider>
+        <Layout className="site-layout">
+          <Header className="site-layout-background" style={{ padding: 0 }} />
+          <Content style={{ margin: "0 16px" }}>
+            <Breadcrumb style={{ margin: "16px 0" }}>
+              <Breadcrumb.Item>User</Breadcrumb.Item>
+              <Breadcrumb.Item>Bill</Breadcrumb.Item>
+            </Breadcrumb>
+            <div
+              className="site-layout-background"
+              style={{ padding: 24, minHeight: 360 }}
+            >
+              Bill is a cat.
+            </div>
+          </Content>
+          <Footer style={{ textAlign: "center" }}>
+            Hong Ly Tech ©2020 Created by Hong
+          </Footer>
+        </Layout>
+      </Layout>
+    </div>
   );
 };
 
-render(<Faker />, document.querySelector("#root"));
+// class SiderDemo extends React.Component {
+//   state = {
+//     collapsed: false,
+//   };
+
+//   onCollapse = (collapsed) => {
+//     console.log(collapsed);
+//     this.setState({ collapsed });
+//   };
+
+//   render() {
+//     return (
+//       <Layout style={{ minHeight: "100vh" }}>
+//         <Sider
+//           collapsible
+//           collapsed={this.state.collapsed}
+//           onCollapse={this.onCollapse}
+//         >
+//           <div className="logo" />
+//           <Menu theme="dark" defaultSelectedKeys={["1"]} mode="inline">
+//             <Menu.Item key="1" icon={<PieChartOutlined />}>
+//               Option 1
+//             </Menu.Item>
+//             <Menu.Item key="2" icon={<DesktopOutlined />}>
+//               Option 2
+//             </Menu.Item>
+//             <SubMenu key="sub1" icon={<UserOutlined />} title="User">
+//               <Menu.Item key="3">Tom</Menu.Item>
+//               <Menu.Item key="4">Bill</Menu.Item>
+//               <Menu.Item key="5">Alex</Menu.Item>
+//             </SubMenu>
+//             <SubMenu key="sub2" icon={<TeamOutlined />} title="Team">
+//               <Menu.Item key="6">Team 1</Menu.Item>
+//               <Menu.Item key="8">Team 2</Menu.Item>
+//             </SubMenu>
+//             <Menu.Item key="9" icon={<FileOutlined />} />
+//           </Menu>
+//         </Sider>
+//         <Layout className="site-layout">
+//           <Header className="site-layout-background" style={{ padding: 0 }} />
+//           <Content style={{ margin: "0 16px" }}>
+//             <Breadcrumb style={{ margin: "16px 0" }}>
+//               <Breadcrumb.Item>User</Breadcrumb.Item>
+//               <Breadcrumb.Item>Bill</Breadcrumb.Item>
+//             </Breadcrumb>
+//             <div
+//               className="site-layout-background"
+//               style={{ padding: 24, minHeight: 360 }}
+//             >
+//               Bill is a cat.
+//             </div>
+//           </Content>
+//           <Footer style={{ textAlign: "center" }}>
+//             Hong Ly Tech ©2020 Created by Hong
+//           </Footer>
+//         </Layout>
+//       </Layout>
+//     );
+//   }
+// }
+
+render(<AntDesign />, document.querySelector("#root"));
