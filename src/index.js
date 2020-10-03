00aead228983d52ecf12f4c1a8ee02d17de2b0ad
lyhd <lyhong.rupp@gmail.com>
2020-10-03 18:43:17 +1000

    built base64 decoder

--- src/index.js
+++ src/index.js
@@ -31,6 +31,22 @@ const EncodeBase64 = () => {
 
   // encodeFileBase64(selectetdFile[0]);
 
+  const decodeFileBase64 = (base64String) => {
+    // From Bytestream to Percent-encoding to Original string
+    return decodeURIComponent(
+      atob(base64String)
+        .split("")
+        .map(function (c) {
+          return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+        })
+        .join("")
+    );
+  };
+
+  const decodeBase64 = decodeFileBase64(
+    fileBase64String.substring(fileBase64String.indexOf(",") + 1)
+  );
+
   return (
     <div>
       <div
@@ -50,9 +66,36 @@ const EncodeBase64 = () => {
       <br />
       <input type="file" id="input" onChange={onFileChange} />
       <TextareaAutosize
+        style={{ width: "500px" }}
         maxRows={20}
         value={fileBase64String}
         onChange={encodeFileBase64(selectetdFile[0])}
+      />
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: "#D50201",
+          height: "60px",
+          width: "100%",
+          color: "#FFF",
+          fontSize: "30px",
+        }}
+      >
+        Files/Image Base64 Decoding
+      </div>
+      <br />
+      <TextareaAutosize
+        style={{ width: "500px" }}
+        maxRows={20}
+        value={fileBase64String}
+      />{" "}
+      <TextareaAutosize
+        style={{ width: "500px" }}
+        maxRows={20}
+        value={decodeBase64}
+        onChange={decodeBase64}
       />
     </div>
   );
