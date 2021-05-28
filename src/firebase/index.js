07e849382f28c2d503f5ddcf24a9b6888330f795
lyhd <lyhong.rupp@gmail.com>
2021-05-28 19:05:10 +1000

    updated to allow upload multiple images to firebase

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -2,14 +2,14 @@ import firebase from "firebase/app";
 import "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBfOh7yoqgGMrMsTxoKoMhP6_9bkUDP0i0",
-  authDomain: "fir-react-upload-5daa4.firebaseapp.com",
-  databaseURL: "https://fir-react-upload-5daa4.firebaseio.com",
-  projectId: "fir-react-upload-5daa4",
-  storageBucket: "fir-react-upload-5daa4.appspot.com",
-  messagingSenderId: "320567469168",
-  appId: "1:320567469168:web:32ea5afe821f2b2cc800b5",
-  measurementId: "G-BLDN7GB52Y"
+  apiKey: "AIzaSyCU12aX4cMSVDBe6x7p6PHiX_h2cSyRHkI",
+  authDomain: "fir-react-image-uploads.firebaseapp.com",
+  databaseURL: "gs://fir-react-image-uploads.appspot.com",
+  projectId: "fir-react-image-uploads",
+  storageBucket: "fir-react-image-uploads.appspot.com",
+  messagingSenderId: "514360265964",
+  appId: "1:514360265964:web:8e6201279b65d6362ce9c9",
+  measurementId: "G-BWJQPEKDQ5",
 };
 
 firebase.initializeApp(firebaseConfig);
--- src/index.js
+++ src/index.js
@@ -3,54 +3,78 @@ import { render } from "react-dom";
 import { storage } from "./firebase";
 
 const ReactFirebaseFileUpload = () => {
-  const [image, setImage] = useState(null);
-  const [url, setUrl] = useState("");
+  const [images, setImages] = useState([]);
+  const [urls, setUrls] = useState([]);
   const [progress, setProgress] = useState(0);
 
-  const handleChange = e => {
-    if (e.target.files[0]) {
-      setImage(e.target.files[0]);
+  const handleChange = (e) => {
+    for (let i = 0; i < e.target.files.length; i++) {
+      const newImage = e.target.files[i];
+      newImage["id"] = Math.random();
+      setImages((prevState) => [...prevState, newImage]);
     }
   };
 
   const handleUpload = () => {
-    const uploadTask = storage.ref(`images/${image.name}`).put(image);
-    uploadTask.on(
-      "state_changed",
-      snapshot => {
-        const progress = Math.round(
-          (snapshot.bytesTransferred / snapshot.totalBytes) * 100
-        );
-        setProgress(progress);
-      },
-      error => {
-        console.log(error);
-      },
-      () => {
-        storage
-          .ref("images")
-          .child(image.name)
-          .getDownloadURL()
-          .then(url => {
-            setUrl(url);
-          });
-      }
-    );
+    const promises = [];
+    images.map((image) => {
+      const uploadTask = storage.ref(`images/${image.name}`).put(image);
+      promises.push(uploadTask);
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          const progress = Math.round(
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+          );
+          setProgress(progress);
+        },
+        (error) => {
+          console.log(error);
+        },
+        async () => {
+          await storage
+            .ref("images")
+            .child(image.name)
+            .getDownloadURL()
+            .then((urls) => {
+              setUrls((prevState) => [...prevState, urls]);
+            });
+        }
+      );
+    });
+
+    Promise.all(promises)
+      .then(() => alert("All images uploaded"))
+      .catch((err) => console.log(err));
   };
 
-  console.log("image: ", image);
+  console.log("images: ", images);
+  console.log("urls", urls);
 
   return (
     <div>
       <progress value={progress} max="100" />
       <br />
       <br />
-      <input type="file" onChange={handleChange} />
+      <input type="file" multiple onChange={handleChange} />
       <button onClick={handleUpload}>Upload</button>
       <br />
-      {url}
+      {urls.map((url, i) => (
+        <div key={i}>
+          <a href={url} target="_blank">
+            {url}
+          </a>
+        </div>
+      ))}
       <br />
-      <img src={url || "http://via.placeholder.com/300"} alt="firebase-image" />
+      {urls.map((url, i) => (
+        <img
+          key={i}
+          style={{ width: "500px" }}
+          src={url || "http://via.placeholder.com/300"}
+          alt="firebase-image"
+        />
+      ))}
     </div>
   );
 };
